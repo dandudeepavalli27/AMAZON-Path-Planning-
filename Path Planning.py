ab9066eea3cc97ab597aeff11f2a5f0360ac1fef
Deepavalli <dandudeepa27@gmail.com>
2026-04-03 11:36:24 +0530

    Create Path Planning.py

---
+++ Path Planning.py
@@ -0,0 +1,13 @@
+# SESSION 16 – Path Planning (Minimum Cost)
+
+# Step 1: Input path costs
+pathA = 6
+pathB = 4
+
+# Step 2: Compare costs
+if pathA < pathB:
+    print("Path A Selected")
+else:
+    print("Path B Selected")
+
+print("\nProgram Executed Successfully")
